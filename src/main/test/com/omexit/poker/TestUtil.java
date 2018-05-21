91a866b34af35a16c5971f1f6c679b814ab8b0ff
Antony Omeri <antony.omeri@jumo.world>
2018-05-21 17:31:19 +0300

    Added tests for util class

--- src/main/test/com/omexit/poker/TestUtil.java
+++ src/main/test/com/omexit/poker/TestUtil.java
@@ -1,4 +1,44 @@
 package com.omexit.poker;
 
-public class TestUtil {
+import junit.framework.TestCase;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TestUtil extends TestCase {
+
+    private String input;
+
+    protected void setUp() throws NoSuchMethodException {
+        input = "AS, 10C, 10H, 3D, 3S";
+    }
+
+    public void testGetHand() {
+        Hand output = Util.getHand(input);
+
+        Hand expectedOuput = new Hand();
+        Card card =new Card("a","s");
+        expectedOuput.addCard(card);
+
+        card =new Card("10","c");
+        expectedOuput.addCard(card);
+
+        card =new Card("10","h");
+        expectedOuput.addCard(card);
+
+        card =new Card("3","d");
+        expectedOuput.addCard(card);
+
+        card =new Card("3","s");
+        expectedOuput.addCard(card);
+
+        assertEquals(expectedOuput.toString(),output.toString());
+    }
+
+    public void testRemoveLastChar() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method removeLastChar = Util.class.getDeclaredMethod("removeLastChar", String.class);
+        removeLastChar.setAccessible(true);
+        String output = (String) removeLastChar.invoke(Util.class, "10S");
+        assertEquals("10", output);
+    }
 }
