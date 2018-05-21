0afd12068416116520e85ece79b5184ac5d385ba
Antony Omeri <antony.omeri@jumo.world>
2018-05-21 15:59:19 +0300

    Refactore util functions

--- src/main/java/com/omexit/poker/Main.java
+++ src/main/java/com/omexit/poker/Main.java
@@ -21,22 +21,7 @@ public class Main {
                 System.out.println("Exiting poker!");
                 exit = true;
             } else {
-                String[] strCards = input.split(",");
-                Hand hand = new Hand();
-
-                for (String strCard : strCards) {
-                    Card card = null;
-                    strCard = strCard.trim();
-                    if(strCard.length() < 2 || strCard.length() > 3){
-                        throw new IllegalArgumentException("Invalid input: " + strCard);
-                    } else {
-                        // Suit is always the last one character
-                        String suit = strCard.substring(strCard.length() - 1).toLowerCase();
-                        String rank = removeLastChar(strCard).toLowerCase();
-                        card = new Card(rank, suit);
-                        hand.addCard(card);
-                    }
-                }
+                Hand hand = Util.getHand(input);
 
                 hand.displayPokerHand();
             }
@@ -45,9 +30,5 @@ public class Main {
         }
     }
 
-    private static String removeLastChar(String s) {
-        return (s == null || s.length() == 0)
-                ? null
-                : (s.substring(0, s.length() - 1));
-    }
+
 }
---
+++ src/main/java/com/omexit/poker/Util.java
@@ -0,0 +1,29 @@
+package com.omexit.poker;
+
+public class Util {
+    public static Hand getHand(String input) {
+        String[] strCards = input.split(",");
+        Hand hand = new Hand();
+
+        for (String strCard : strCards) {
+            Card card = null;
+            strCard = strCard.trim();
+            if(strCard.length() < 2 || strCard.length() > 3){
+                throw new IllegalArgumentException("Invalid input: " + strCard);
+            } else {
+                // Suit is always the last one character
+                String suit = strCard.substring(strCard.length() - 1).toLowerCase();
+                String rank = removeLastChar(strCard).toLowerCase();
+                card = new Card(rank, suit);
+                hand.addCard(card);
+            }
+        }
+        return hand;
+    }
+
+    private static String removeLastChar(String s) {
+        return (s == null || s.length() == 0)
+                ? null
+                : (s.substring(0, s.length() - 1));
+    }
+}
