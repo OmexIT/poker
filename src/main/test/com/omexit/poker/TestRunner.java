40a739884e3ebb06518155e77512333ffc7bf02b
Antony Omeri <antony.omeri@jumo.world>
2018-05-21 17:31:48 +0300

    Added test runner

--- src/main/test/com/omexit/poker/TestRunner.java
+++ src/main/test/com/omexit/poker/TestRunner.java
@@ -1,4 +1,13 @@
 package com.omexit.poker;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestHand.class,
+        TestUtil.class
+})
 public class TestRunner {
+
 }
